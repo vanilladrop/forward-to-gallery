6afa24bbb68e11329611a9151b5d8ac556920151
vanilladrop <victorfc_1804@hotmail.com>
2024-02-14 00:37:33 -0600

    Add logic to show a different UI depending on if app was launched fresh or launched by sharing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,40 +18,88 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
-      home: const Homepage(title: 'Forward to Gallery'),
+      home: SharedItemPage(),
     );
   }
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key, required this.title});
-
-  final String title;
-
+class SharedItemPage extends StatefulWidget {
   @override
-  State<Homepage> createState() => _HomepageState();
+  _SharedItemPageState createState() => _SharedItemPageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _SharedItemPageState extends State<SharedItemPage> {
+  late StreamSubscription _intentSubscription;
+  final _sharedMedia = <SharedMediaFile>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen to media sharing coming from outside the app while the app is in the memory.
+    _intentSubscription = ReceiveSharingIntent.getMediaStream().listen((value) {
+      setState(() {
+        _sharedMedia.clear();
+        _sharedMedia.addAll(value);
+
+        print(_sharedMedia.map((media) => media.toMap()));
+      });
+    }, onError: (error) {
+      print("getIntentDataStream error: $error");
+    });
+
+    // Get the media sharing coming from outside the app while the app is closed.
+    ReceiveSharingIntent.getInitialMedia().then((media) => {
+          setState(() {
+            _sharedMedia.clear();
+            _sharedMedia.addAll(media);
+            print(_sharedMedia.map((media) => media.toMap()));
+
+            // Tell the library that we are done processing the intent.
+            ReceiveSharingIntent.reset();
+          })
+        });
+  }
+
+  @override
+  void dispose() {
+    _intentSubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Share Intent"),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                'This app is intended to be used via your phone\'s sharing/forwarding functionality.\n\nTry sharing an image or video file to this app!',
-                style: TextStyle(fontSize: 25.0),
-                textAlign: TextAlign.center,
-              ),
-            ),
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  //If there's no shared media, we assume the app was launched by itself. Thus, we tell the user to share media to it.
+                  //If there's media, we display the path to it.
+                  children: _sharedMedia.isEmpty
+                      ? <Widget>[
+                          const Text(
+                            'This app is intended to be used via your phone\'s sharing/forwarding functionality.\n\nTry sharing an image or video file to this app!',
+                            style: TextStyle(fontSize: 25.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ]
+                      : <Widget>[
+                          const Text('Shared media:',
+                              style: TextStyle(fontSize: 25.0),
+                              textAlign: TextAlign.center),
+                          Text(_sharedMedia
+                              .map((files) => files.toMap())
+                              .join(",\n----------\n"))
+                        ],
+                )),
           ],
         ),
       ),
